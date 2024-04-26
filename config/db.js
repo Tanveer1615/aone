0c460add3af8280e2c725477e57aeb1a60c674db
Tanveer <ta91507@gmail.com>
2024-04-26 23:11:02 +0530

    Alct

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const DB_OPTIONS = {
+      dbName: "Tanveer",
+    };
+    await mongoose.connect(process.env.MONGODB_URI, DB_OPTIONS);
   } catch (err) {
     console.log(err);
   }
